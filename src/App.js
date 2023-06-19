6b5232f8341d8cfe9ca8e6b4f623b9b24875df30
BOSTONE069 <bostoneochieng69@gmail.com>
2023-06-18 20:13:02 -0400

    Added comments to the components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import './App.css';
 import Greeting from './components/Greet';
 
+/**
+ * The function returns a component called "App" that renders a "Greeting" component.
+ * @returns The `App` component is being returned, which contains a `div` element with a class name of
+ * "App" and a `Greeting` component.
+ */
 function App() {
   return (
     <div className="App">
--- src/components/Greet.js
+++ src/components/Greet.js
@@ -7,6 +7,12 @@ import React from 'react';
 //             </div>
 //         );
 // }
+/**
+ * The above code defines a functional component called Greeting that returns a div containing a
+ * heading with the text "Hello".
+ * @returns A React functional component named `Greeting` is being returned. It renders a `div` element
+ * containing an `h1` element with the text "Hello".
+ */
 const Greeting = () => {
     return (
         <div>
